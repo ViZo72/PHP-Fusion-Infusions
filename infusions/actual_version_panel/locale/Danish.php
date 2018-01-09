45d10281c0c2e2564cccdd164227e97c08342581
Helmuth Mikkelsen <helmuthm@gmail.com>
2018-01-09 10:36:16 +0100

    Added Danish.php
    Danish locales

---
+++ infusions/actual_version_panel/locale/Danish.php
@@ -0,0 +1,29 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: actual_version_panel/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['AV_title'] = 'Aktuelle Version';
+$locale['AV_desc'] = 'Aktuelle Version Panel';
+$locale['AV_001'] = 'Download';
+$locale['AV_002'] = 'Oversættelse';
+// Administration
+$locale['AV_100'] = 'Aktuelle PHP-Fusion Version';
+$locale['AV_101'] = 'Downloadlink til PHP-Fusion';
+$locale['AV_102'] = 'Downloadlink til Oversættelse';
+$locale['AV_103'] = 'PHP-Fusion er et letvægts open source content management system (CMS) skrevet i PHP.
+PHP-Fusion anvender en MySQL-database til at opbevare din hjemmesides indhold og inkluderer et enkelt men alligevel omfattende administrationsmodul til at vedligeholde dine hjemmesider.
+PHP-Fusion indeholder de mest almindelige features du også vil kunne finde i mange andre content management system pakker.';
+$locale['AV_notice'] = 'Indstillinger er blevet opdateret';
