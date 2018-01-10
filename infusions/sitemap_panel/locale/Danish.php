83dbf42baa1f9c60ad9bc772a83edfe1396df4da
Helmuth Mikkelsen <helmuthm@gmail.com>
2018-01-10 10:32:04 +0100

    New locales & 1 update
    infusions/content_creator/locale/Danish.php
    infusions/sitemap_panel/locale/Danish.php
    infusions/team/locale/Danish.php
    infusions/theme_switcher_panel/locale/Danish.php

---
+++ infusions/sitemap_panel/locale/Danish.php
@@ -0,0 +1,58 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: sitemap/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['SMG_title'] = 'Sitemap';
+$locale['SMG_desc'] = 'XML Sitemap generator';
+$locale['SMG_title_admin'] = 'Sitemap Generator';
+$locale['SMG_001'] = 'Sitemap oprettet d.:';
+$locale['SMG_002'] = 'Vis sitemap.xml';
+$locale['SMG_003'] = 'Vælg alt';
+$locale['SMG_004'] = 'Opret';
+$locale['SMG_005'] = 'URL-adresse';
+$locale['SMG_006'] = 'Skift hyppighed';
+$locale['SMG_007'] = 'Altid';
+$locale['SMG_008'] = 'Time';
+$locale['SMG_009'] = 'Daglig';
+$locale['SMG_010'] = 'Ugentlig';
+$locale['SMG_011'] = 'Månedligt';
+$locale['SMG_012'] = 'Årligt';
+$locale['SMG_013'] = 'Aldrig';
+$locale['SMG_014'] = 'Prioritering';
+$locale['SMG_015'] = 'Auto opdater';
+$locale['SMG_016'] = 'Auto opdater interval (timer)';
+$locale['SMG_type_01'] = 'Brugeroprettede links';
+$locale['SMG_type_02'] = 'Profiler';
+$locale['SMG_type_03'] = 'Profiler';
+$locale['SMG_type_04'] = 'Artikel kategorier';
+$locale['SMG_type_05'] = 'Blogs';
+$locale['SMG_type_06'] = 'Blog kategorier';
+$locale['SMG_type_07'] = 'Brugeroprettede sider';
+$locale['SMG_type_08'] = 'Downloads';
+$locale['SMG_type_09'] = 'Download kategorier';
+$locale['SMG_type_10'] = 'FAQ kategorier';
+$locale['SMG_type_11'] = 'Debatforum';
+$locale['SMG_type_12'] = 'Fotos';
+$locale['SMG_type_13'] = 'Fotoalbums';
+$locale['SMG_type_14'] = 'Nyheder';
+$locale['SMG_type_15'] = 'Nyheds kategorier';
+$locale['SMG_type_16'] = 'Links';
+$locale['SMG_type_17'] = 'Links kategorier';
+$locale['SMG_notice_001'] = 'Link er blevet opdateret';
+$locale['SMG_notice_002'] = 'Link er blevet tilføjet';
+$locale['SMG_notice_003'] = 'Filen er blevet oprettet';
+$locale['SMG_notice_04'] = 'Ændringer er blevet gemt';
+$locale['SMG_notice_05'] = 'Indstillinger er blevet gemt';
---
+++ infusions/team/locale/Danish.php
@@ -0,0 +1,31 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: team/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['TEAM_title'] = 'Team';
+$locale['TEAM_desc'] = 'Team Liste';
+$locale['TEAM_title_admin'] = 'Team Admin';
+$locale['TEAM_001'] = 'Navn';
+$locale['TEAM_002'] = 'Position';
+$locale['TEAM_003'] = 'Erhverv';
+$locale['TEAM_004'] = 'Tilmeldt';
+$locale['TEAM_005'] = 'PB';
+$locale['TEAM_006'] = 'Handlinger';
+$locale['TEAM_007'] = 'Ingen brugere';
+$locale['TEAM_008'] = 'Vælg bruger';
+$locale['TEAM_009'] = 'Bruger tilføjet';
+$locale['TEAM_010'] = 'Bruger opdateret';
+$locale['TEAM_011'] = 'Bruger slettet';
