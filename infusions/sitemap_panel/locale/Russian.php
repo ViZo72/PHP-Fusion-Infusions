6724b9b4e8bc7f9c6a819779aaa677dab8f7f20e
RobiNN <RobiNN1@users.noreply.github.com>
2018-01-16 15:37:27 +0100

    Sitemap - add Russian locale

---
+++ infusions/sitemap_panel/locale/Russian.php
@@ -0,0 +1,59 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: sitemap_panel/locale/Russian.php
+| Author: zizub
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['SMG_title'] = 'Sitemap';
+$locale['SMG_desc'] = 'XML Sitemap generator';
+$locale['SMG_title_admin'] = 'Sitemap Generator';
+$locale['SMG_001'] = 'Время создания Sitemap:';
+$locale['SMG_002'] = 'Показать файл sitemap.xml';
+$locale['SMG_003'] = 'Выбрать все';
+$locale['SMG_004'] = 'Генерация Google XML Sitemap';
+$locale['SMG_005'] = 'URL-адрес страницы';
+$locale['SMG_006'] = 'Изменить частоту:';
+$locale['SMG_007'] = 'Всегда';
+$locale['SMG_008'] = 'Ежечасно';
+$locale['SMG_009'] = 'Ежедневно';
+$locale['SMG_010'] = 'Еженедельно';
+$locale['SMG_011'] = 'Ежемесячно';
+$locale['SMG_012'] = 'Ежегодно';
+$locale['SMG_013'] = 'Никогда';
+$locale['SMG_014'] = 'Приоритет:';
+$locale['SMG_015'] = 'Автоматическое обновление';
+$locale['SMG_016'] = 'Интервал автоматического обновления (часы)';
+$locale['SMG_type_01'] = 'Пользовательские страницы';
+$locale['SMG_type_02'] = 'Профили';
+$locale['SMG_type_03'] = 'Статьи';
+$locale['SMG_type_04'] = 'Категории статей';
+$locale['SMG_type_05'] = 'Блоги';
+$locale['SMG_type_06'] = 'Категории блогов';
+$locale['SMG_type_07'] = 'Пользовательские страницы';
+$locale['SMG_type_08'] = 'Загрузки';
+$locale['SMG_type_09'] = 'Категории загрузок';
+$locale['SMG_type_10'] = 'Категории FAQ';
+$locale['SMG_type_11'] = 'Форум';
+$locale['SMG_type_12'] = 'Фотографии';
+$locale['SMG_type_13'] = 'Фотоальбомы';
+$locale['SMG_type_14'] = 'Новости';
+$locale['SMG_type_15'] = 'Категории новостей';
+$locale['SMG_type_16'] = 'Веб-ссылки';
+$locale['SMG_type_17'] = 'Категории веб-ссылок';
+$locale['SMG_notice_01'] = 'Ссылка успешно обновлена!';
+$locale['SMG_notice_02'] = 'Ссылка успешно добавлена!';
+$locale['SMG_notice_03'] = 'Карта сайта успешно создана!';
+$locale['SMG_notice_04'] = 'Изменения успешно сохранены!';
+$locale['SMG_notice_05'] = 'Настройки успешно сохранены!';
+$locale['SMG_notice_06'] = 'Ссылка успешно удалена!';
