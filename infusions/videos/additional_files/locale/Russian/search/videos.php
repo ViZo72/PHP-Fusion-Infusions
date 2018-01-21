7827eb68265da47197ccc3245f79afdd57a82ee2
RobiNN <RobiNN1@users.noreply.github.com>
2018-01-21 10:43:11 +0100

    Add Russian locale

---
+++ infusions/videos/additional_files/locale/Russian/search/videos.php
@@ -0,0 +1,22 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: locale/Russian/search/videos.php
+| Author: zizub
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['v400'] = 'Видео';
+$locale['v401'] = 'Видео';
+$locale['v402'] = 'Видео';
+$locale['v403'] = 'Дата добавления:';
+$locale['v404'] = 'Просмотры:';
