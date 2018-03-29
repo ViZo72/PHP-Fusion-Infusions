b045b2339b3b5acbb4cf48d11a5f534860ca8c77
RobiNN <RobiNN1@users.noreply.github.com>
2018-03-29 18:41:04 +0200

    Update panels

--- infusions/most_read_blogs_panel/most_read_blogs_panel.php
+++ infusions/most_read_blogs_panel/most_read_blogs_panel.php
@@ -20,7 +20,7 @@ if (!defined('IN_FUSION')) {
     die('Access Denied');
 }
 
-if (file_exists(INFUSIONS.'most_read_blogs_panel/locale/'.LANGUAGE.'php')) {
+if (file_exists(INFUSIONS.'most_read_blogs_panel/locale/'.LANGUAGE.'.php')) {
     $locale = fusion_get_locale('', INFUSIONS.'most_read_blogs_panel/locale/'.LANGUAGE.'.php');
 } else {
     $locale = fusion_get_locale('', INFUSIONS.'most_read_blogs_panel/locale/English.php');
@@ -31,30 +31,34 @@ add_to_head('<style type="text/css">
 .most-read-blogs .nav-pills>li>a {padding: 0 5px;}
 .most-read-blogs .nav-pills>li>a:hover,.most-read-blogs .nav-pills>li>a:focus {background-color: transparent;color: inherit;}
 .most-read-blogs .nav-pills>li.active>a,.most-read-blogs .nav-pills>li.active>a:hover,.most-read-blogs .nav-pills>li.active>a:focus {background-color: transparent;color: inherit;font-weight: bold;}
+.most-read-blogs hr {margin-top: 5px;margin-bottom: 5px;}
 </style>');
 
 openside($locale['MRB_01']);
 echo '<div class="most-read-blogs">';
     echo '<ul class="nav nav-pills text-center" role="tablist">';
-        echo '<li role="presentation" class="active"><a href="#days7" aria-controls="days7" role="tab" data-toggle="tab">'.$locale['MRB_02'].'</a></li>';
-        echo '<li role="presentation"><a href="#days14" aria-controls="days14" role="tab" data-toggle="tab">'.$locale['MRB_03'].'</a></li>';
-        echo '<li role="presentation"><a href="#days30" aria-controls="days30" role="tab" data-toggle="tab">'.$locale['MRB_04'].'</a></li>';
+        echo '<li role="presentation" class="active"><a href="#blogs-7days" aria-controls="blogs-7days" role="tab" data-toggle="tab">'.$locale['MRB_02'].'</a></li>';
+        echo '<li role="presentation"><a href="#blogs-14days" aria-controls="blogs-14days" role="tab" data-toggle="tab">'.$locale['MRB_03'].'</a></li>';
+        echo '<li role="presentation"><a href="#blogs-30days" aria-controls="blogs-30days" role="tab" data-toggle="tab">'.$locale['MRB_04'].'</a></li>';
     echo '</ul>';
 
     echo '<div class="tab-content m-t-5">';
-        echo '<div role="tabpanel" class="tab-pane active" id="days7">';
-            $result_7days = dbquery("SELECT blog_id, blog_subject, blog_reads, blog_datestamp, blog_start, blog_end, blog_draft
+        echo '<div role="tabpanel" class="tab-pane active" id="blogs-7days">';
+            $result_7days = dbquery("SELECT blog_id, blog_subject, blog_reads, blog_datestamp, blog_draft
                 FROM ".DB_BLOG."
-                WHERE (".time()." > blog_start OR blog_start = 0) AND blog_draft = 0 AND (".time()." < blog_end OR blog_end = 0) AND blog_draft = 0 AND blog_datestamp > '".strtotime('-7 day')."'
+                WHERE blog_draft = 0 AND blog_datestamp > '".strtotime('-7 day')."'
                 ".(multilang_table('BL') ? " AND blog_language='".LANGUAGE."' AND " : '').groupaccess('blog_visibility')."
                 ORDER BY blog_reads DESC
                 LIMIT 15
             ");
 
             if (dbrows($result_7days) > 0) {
-                echo '<ul>';
+                echo '<ul class="blogs-list list-style-none">';
+                $i = 0;
                 while ($data = dbarray($result_7days)) {
-                    echo '<li><a href="'.INFUSIONS.'blog/blog.php?readmore='.$data['blog_id'].'">'.$data['blog_subject'].'</a> <span class="badge">'.$data['blog_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    echo $i > 0 ? '<hr/>' : '';
+                    echo '<li><a href="'.INFUSIONS.'blog/blog.php?readmore='.$data['blog_id'].'">'.$data['blog_subject'].'</a> <span class="badge pull-right">'.$data['blog_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    $i++;
                 }
                 echo '</ul>';
             } else {
@@ -63,19 +67,22 @@ echo '<div class="most-read-blogs">';
 
         echo '</div>';
 
-        echo '<div role="tabpanel" class="tab-pane" id="days14">';
-            $result_14days = dbquery("SELECT blog_id, blog_subject, blog_reads, blog_datestamp, blog_start, blog_end, blog_draft
+        echo '<div role="tabpanel" class="tab-pane" id="blogs-14days">';
+            $result_14days = dbquery("SELECT blog_id, blog_subject, blog_reads, blog_datestamp, blog_draft
                 FROM ".DB_BLOG."
-                WHERE (".time()." > blog_start OR blog_start = 0) AND blog_draft = 0 AND (".time()." < blog_end OR blog_end = 0) AND blog_draft = 0 AND blog_datestamp > '".strtotime('-14 day')."'
+                WHERE blog_draft = 0 AND blog_datestamp > '".strtotime('-14 day')."'
                 ".(multilang_table('BL') ? " AND blog_language='".LANGUAGE."' AND " : '').groupaccess('blog_visibility')."
                 ORDER BY blog_reads DESC
                 LIMIT 15
             ");
 
             if (dbrows($result_14days) > 0) {
-                echo '<ul>';
+                echo '<ul class="blogs-list list-style-none">';
+                $i = 0;
                 while ($data = dbarray($result_14days)) {
-                    echo '<li><a href="'.INFUSIONS.'blog/blog.php?readmore='.$data['blog_id'].'">'.$data['blog_subject'].'</a> <span class="badge">'.$data['blog_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    echo $i > 0 ? '<hr/>' : '';
+                    echo '<li><a href="'.INFUSIONS.'blog/blog.php?readmore='.$data['blog_id'].'">'.$data['blog_subject'].'</a> <span class="badge pull-right">'.$data['blog_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    $i++;
                 }
                 echo '</ul>';
             } else {
@@ -83,19 +90,22 @@ echo '<div class="most-read-blogs">';
             }
         echo '</div>';
 
-        echo '<div role="tabpanel" class="tab-pane" id="days30">';
-            $result_30days = dbquery("SELECT blog_id, blog_subject, blog_reads, blog_datestamp, blog_start, blog_end, blog_draft
+        echo '<div role="tabpanel" class="tab-pane" id="blogs-30days">';
+            $result_30days = dbquery("SELECT blog_id, blog_subject, blog_reads, blog_datestamp, blog_draft
                 FROM ".DB_BLOG."
-                WHERE (".time()." > blog_start OR blog_start = 0) AND blog_draft = 0 AND (".time()." < blog_end OR blog_end = 0) AND blog_draft = 0 AND blog_datestamp > '".strtotime('-30 day')."'
+                WHERE blog_draft = 0 AND blog_datestamp > '".strtotime('-30 day')."'
                 ".(multilang_table('BL') ? " AND blog_language='".LANGUAGE."' AND " : '').groupaccess('blog_visibility')."
                 ORDER BY blog_reads DESC
                 LIMIT 15
             ");
 
             if (dbrows($result_30days) > 0) {
-                echo '<ul>';
+                echo '<ul class="blogs-list list-style-none">';
+                $i = 0;
                 while ($data = dbarray($result_30days)) {
-                    echo '<li><a href="'.INFUSIONS.'blog/blog.php?readmore='.$data['blog_id'].'">'.$data['blog_subject'].'</a> <span class="badge">'.$data['blog_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    echo $i > 0 ? '<hr/>' : '';
+                    echo '<li><a href="'.INFUSIONS.'blog/blog.php?readmore='.$data['blog_id'].'">'.$data['blog_subject'].'</a> <span class="badge pull-right">'.$data['blog_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    $i++;
                 }
                 echo '</ul>';
             } else {
--- infusions/most_read_news_panel/most_read_news_panel.php
+++ infusions/most_read_news_panel/most_read_news_panel.php
@@ -20,7 +20,7 @@ if (!defined('IN_FUSION')) {
     die('Access Denied');
 }
 
-if (file_exists(INFUSIONS.'most_read_news_panel/locale/'.LANGUAGE.'php')) {
+if (file_exists(INFUSIONS.'most_read_news_panel/locale/'.LANGUAGE.'.php')) {
     $locale = fusion_get_locale('', INFUSIONS.'most_read_news_panel/locale/'.LANGUAGE.'.php');
 } else {
     $locale = fusion_get_locale('', INFUSIONS.'most_read_news_panel/locale/English.php');
@@ -31,30 +31,34 @@ add_to_head('<style type="text/css">
 .most-read-news .nav-pills>li>a {padding: 0 5px;}
 .most-read-news .nav-pills>li>a:hover,.most-read-news .nav-pills>li>a:focus {background-color: transparent;color: inherit;}
 .most-read-news .nav-pills>li.active>a,.most-read-news .nav-pills>li.active>a:hover,.most-read-news .nav-pills>li.active>a:focus {background-color: transparent;color: inherit;font-weight: bold;}
+.most-read-news hr {margin-top: 5px;margin-bottom: 5px;}
 </style>');
 
 openside($locale['MRN_01']);
 echo '<div class="most-read-news">';
     echo '<ul class="nav nav-pills text-center" role="tablist">';
-        echo '<li role="presentation" class="active"><a href="#days7" aria-controls="days7" role="tab" data-toggle="tab">'.$locale['MRN_02'].'</a></li>';
-        echo '<li role="presentation"><a href="#days14" aria-controls="days14" role="tab" data-toggle="tab">'.$locale['MRN_03'].'</a></li>';
-        echo '<li role="presentation"><a href="#days30" aria-controls="days30" role="tab" data-toggle="tab">'.$locale['MRN_04'].'</a></li>';
+        echo '<li role="presentation" class="active"><a href="#news-7days" aria-controls="news-7days" role="tab" data-toggle="tab">'.$locale['MRN_02'].'</a></li>';
+        echo '<li role="presentation"><a href="#news-14days" aria-controls="news-14days" role="tab" data-toggle="tab">'.$locale['MRN_03'].'</a></li>';
+        echo '<li role="presentation"><a href="#news-30days" aria-controls="news-30days" role="tab" data-toggle="tab">'.$locale['MRN_04'].'</a></li>';
     echo '</ul>';
 
     echo '<div class="tab-content m-t-5">';
-        echo '<div role="tabpanel" class="tab-pane active" id="days7">';
-            $result_7days = dbquery("SELECT news_id, news_subject, news_reads, news_datestamp, news_start, news_end, news_draft
+        echo '<div role="tabpanel" class="tab-pane active" id="news-7days">';
+            $result_7days = dbquery("SELECT news_id, news_subject, news_reads, news_datestamp, news_draft
                 FROM ".DB_NEWS."
-                WHERE (".time()." > news_start OR news_start = 0) AND news_draft = 0 AND (".time()." < news_end OR news_end = 0) AND news_draft = 0 AND news_datestamp > '".strtotime('-7 day')."'
+                WHERE news_draft = 0 AND news_datestamp > '".strtotime('-7 day')."'
                 ".(multilang_table('NS') ? " AND news_language='".LANGUAGE."' AND " : '').groupaccess('news_visibility')."
                 ORDER BY news_reads DESC
                 LIMIT 15
             ");
 
             if (dbrows($result_7days) > 0) {
-                echo '<ul>';
+                echo '<ul class="news-list list-style-none">';
+                $i = 0;
                 while ($data = dbarray($result_7days)) {
-                    echo '<li><a href="'.INFUSIONS.'news/news.php?readmore='.$data['news_id'].'">'.$data['news_subject'].'</a> <span class="badge">'.$data['news_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    echo $i > 0 ? '<hr/>' : '';
+                    echo '<li><a href="'.INFUSIONS.'news/news.php?readmore='.$data['news_id'].'">'.trim_text($data['news_subject'], 25).'</a> <span class="badge pull-right">'.$data['news_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    $i++;
                 }
                 echo '</ul>';
             } else {
@@ -63,19 +67,22 @@ echo '<div class="most-read-news">';
 
         echo '</div>';
 
-        echo '<div role="tabpanel" class="tab-pane" id="days14">';
-            $result_14days = dbquery("SELECT news_id, news_subject, news_reads, news_datestamp, news_start, news_end, news_draft
+        echo '<div role="tabpanel" class="tab-pane" id="news-14days">';
+            $result_14days = dbquery("SELECT news_id, news_subject, news_reads, news_datestamp, news_draft
                 FROM ".DB_NEWS."
-                WHERE (".time()." > news_start OR news_start = 0) AND news_draft = 0 AND (".time()." < news_end OR news_end = 0) AND news_draft = 0 AND news_datestamp > '".strtotime('-14 day')."'
+                WHERE news_draft = 0 AND news_datestamp > '".strtotime('-14 day')."'
                 ".(multilang_table('NS') ? " AND news_language='".LANGUAGE."' AND " : '').groupaccess('news_visibility')."
                 ORDER BY news_reads DESC
                 LIMIT 15
             ");
 
             if (dbrows($result_14days) > 0) {
-                echo '<ul>';
+                echo '<ul class="news-list list-style-none">';
+                $i = 0;
                 while ($data = dbarray($result_14days)) {
-                    echo '<li><a href="'.INFUSIONS.'news/news.php?readmore='.$data['news_id'].'">'.$data['news_subject'].'</a> <span class="badge">'.$data['news_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    echo $i > 0 ? '<hr/>' : '';
+                    echo '<li><a href="'.INFUSIONS.'news/news.php?readmore='.$data['news_id'].'">'.trim_text($data['news_subject'], 25).'</a> <span class="badge pull-right">'.$data['news_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    $i++;
                 }
                 echo '</ul>';
             } else {
@@ -83,19 +90,22 @@ echo '<div class="most-read-news">';
             }
         echo '</div>';
 
-        echo '<div role="tabpanel" class="tab-pane" id="days30">';
-            $result_30days = dbquery("SELECT news_id, news_subject, news_reads, news_datestamp, news_start, news_end, news_draft
+        echo '<div role="tabpanel" class="tab-pane" id="news-30days">';
+            $result_30days = dbquery("SELECT news_id, news_subject, news_reads, news_datestamp, news_draft
                 FROM ".DB_NEWS."
-                WHERE (".time()." > news_start OR news_start = 0) AND news_draft = 0 AND (".time()." < news_end OR news_end = 0) AND news_draft = 0 AND news_datestamp > '".strtotime('-30 day')."'
+                WHERE news_draft = 0 AND news_datestamp > '".strtotime('-30 day')."'
                 ".(multilang_table('NS') ? " AND news_language='".LANGUAGE."' AND " : '').groupaccess('news_visibility')."
                 ORDER BY news_reads DESC
                 LIMIT 15
             ");
 
             if (dbrows($result_30days) > 0) {
-                echo '<ul>';
+                echo '<ul class="news-list list-style-none">';
+                $i = 0;
                 while ($data = dbarray($result_30days)) {
-                    echo '<li><a href="'.INFUSIONS.'news/news.php?readmore='.$data['news_id'].'">'.$data['news_subject'].'</a> <span class="badge">'.$data['news_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    echo $i > 0 ? '<hr/>' : '';
+                    echo '<li><a href="'.INFUSIONS.'news/news.php?readmore='.$data['news_id'].'">'.trim_text($data['news_subject'], 25).'</a> <span class="badge pull-right">'.$data['news_reads'].' <i class="fa fa-eye"></i></span></li>';
+                    $i++;
                 }
                 echo '</ul>';
             } else {
