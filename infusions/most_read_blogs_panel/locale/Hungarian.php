1f0c13268ec6173ab712171348d8f9ec81d8fd89
RobiNN <RobiNN1@users.noreply.github.com>
2018-04-15 16:04:40 +0200

    Add Hungarian locale

---
+++ infusions/most_read_blogs_panel/locale/Hungarian.php
@@ -0,0 +1,22 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: most_read_blogs_panel/locale/Hungarian.php
+| Author: Karrak
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['MRB_01'] = 'Legolvasottabb Blogok';
+$locale['MRB_02'] = '7 Nap';
+$locale['MRB_03'] = '14 Nap';
+$locale['MRB_04'] = '30 Nap';
+$locale['MRB_05'] = 'Nincs Blog bejegyzés';
